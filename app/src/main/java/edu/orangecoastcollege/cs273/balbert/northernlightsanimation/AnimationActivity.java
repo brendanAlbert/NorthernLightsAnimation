88a75f8835d92a23a2d1f59d56193977ae2048c2
brendan <alber123@mail.chapman.edu>
2017-10-24 11:24:13 -0700

    added the custom_anim.xml  and toggleCustomAnim() method's code.  Completed JavaDocs.

--- app/src/main/java/edu/orangecoastcollege/cs273/balbert/northernlightsanimation/AnimationActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/balbert/northernlightsanimation/AnimationActivity.java
@@ -8,6 +8,13 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+/**
+ * AnimationActivity is the main and only Controller for NorthernLightsAnimation.
+ *
+ * This app has 4 unique animations to apply to the ImageView.
+ *
+ * When a button is tapped, its respective toggleAnim method is called.
+ */
 public class AnimationActivity extends AppCompatActivity {
 
     private AnimationDrawable frameAnim; // used for frame animations
@@ -17,6 +24,11 @@ public class AnimationActivity extends AppCompatActivity {
 
     private ImageView lightsImageView;
 
+    /**
+     * onCreate() sets the content view and wires this controller to the lone ImageView
+     * in the activity_animation.xml View.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +37,16 @@ public class AnimationActivity extends AppCompatActivity {
         lightsImageView = (ImageView) findViewById(R.id.lightsImageView);
     }
 
+    /**
+     * toggleFrameAnim(View view) is called when the Frame Animation button is tapped.
+     *
+     * The first time the button is tapped, the frameAnim AnimationDrawable is null.
+     * The ImageView is then wired to this frameAnim AnimationDrawable.
+     *
+     * If the animation is running and the button is tapped again, it is stopped.
+     * Otherwise, if the frameAnim has been initialized and it is not running, start it.
+     * @param view
+     */
     public void toggleFrameAnim(View view)
     {
         // hasn't been initialized yet
@@ -40,6 +62,19 @@ public class AnimationActivity extends AppCompatActivity {
         else frameAnim.start();
     }
 
+    /**
+     * toggleRotateAnim(View view) is called when the Rotate Animation button is tapped.
+     *
+     * If the rotateAnim Animation object is null, load the appropriate animation code from
+     * rotate_anim.xml in the anim directory.
+     *
+     * If the animation has not started or it has ended, start the animation when the the
+     * Rotate Animation button is tapped.
+     *
+     * Otherwise, since the button has been tapped again, and the animation is in progress,
+     * stop it.
+     * @param view
+     */
     public void toggleRotateAnim(View view)
     {
         // hasn't been initialized yet
@@ -51,14 +86,29 @@ public class AnimationActivity extends AppCompatActivity {
             lightsImageView.clearAnimation();
     }
 
+    /**
+     * toggleShakeAnim(View view) is called when the Shake Animation button is tapped.
+     *
+     * The appropriate xml file is loaded, shake_anim.xml and the animation is started.
+     *
+     * @param view
+     */
     public void toggleShakeAnim(View view)
     {
         shakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake_anim);
         lightsImageView.startAnimation(shakeAnim);
     }
 
+    /**
+     * toggleCustomAnim(View view) is called when the Custom Animation button is tapped.
+     *
+     * The appropriate xml file is loaded, custom_anim.xml and the animation is started.
+     *
+     * @param view
+     */
     public void toggleCustomAnim(View view)
     {
-
+        customAnim = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        lightsImageView.startAnimation(customAnim);
     }
 }
